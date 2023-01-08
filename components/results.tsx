3d58d5f5dbb00dbbbc3ca0c2eaacb7550f487410
glen <glen_dovey@hotmail.com>
2023-01-08 14:48:45 +0000

    removed unnecessary or assignment for keys in result

--- components/results.tsx
+++ components/results.tsx
@@ -2,7 +2,7 @@ import { PositionData } from "../src/types"
 
 export const Results = ({results}: {results:PositionData[]}) => {
     if (results[0]) {
-        const keys = Object.keys(results[0]) || []
+        const keys = Object.keys(results[0])
         return <table>
         <thead>{
             keys.map(key => <th key={key}>{key.toLocaleUpperCase()}</th>)
